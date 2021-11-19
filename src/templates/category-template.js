6bf9f221b753d17c9fd9d7a2d54c46ea04c2b5a7
Sumesh <sumesh.majhi.5@gmail.com>
2021-11-19 11:49:50 +0530

    📦 NEW: Add Categories Query

--- src/templates/category-template.js
+++ src/templates/category-template.js
@@ -4,14 +4,37 @@ import Hero from '../components/Hero'
 import Posts from '../components/Posts'
 import { graphql } from 'gatsby'
 
-const CategoryTemplate = ()=> {
-  
-  return (
-   <h2>category template</h2>
-  )
+const CategoryTemplate = props => {
+  console.log(props)
+
+  return <h2>category template</h2>
 }
 
-
-
+export const query = graphql`
+  query GetCategories($category: String) {
+    allMdx(
+      sort: { fields: frontmatter___date, order: DESC }
+      filter: { frontmatter: { category: { eq: $category } } }
+    ) {
+      nodes {
+        id
+        frontmatter {
+          title
+          author
+          category
+          readTime
+          slug
+          date(formatString: "MMMM, Do YYYY")
+          image {
+            childImageSharp {
+              gatsbyImageData(placeholder: TRACED_SVG)
+            }
+          }
+        }
+        excerpt
+      }
+    }
+  }
+`
 
 export default CategoryTemplate
