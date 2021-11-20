82dd567fb3e2d708f33354652b26e10f07017d5b
Sumesh <sumesh.majhi.5@gmail.com>
2021-11-20 12:29:34 +0530

    📦 NEW: Add Prism Setup

--- root-mdx.js
+++ root-mdx.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { MDXProvider } from '@mdx-js/react'
-import { Headings, Code, Blockquote } from './src/components/Complete'
+import { Headings, Code, Blockquote, Example } from './src/components/Complete'
 
 // const Testing = ({ children }) => {
 //   return <code className="code">{children}</code>
@@ -11,6 +11,7 @@ const components = {
   h3: Headings.myH4,
   inlineCode: Code,
   blockquote: Blockquote,
+  pre: Example,
 }
 
 export const wrapMDX = ({ element }) => {
